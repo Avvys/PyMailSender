b5de52dc7a37b4991ea94121d532725dd05390b1
Avvys <avvys@op.pl>
2014-12-03 09:04:29 +0100

    Sending email works

---
+++ sendmail.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import smtplib
+import json
+import sys
+
+# usage: ./sendmail.py "message to send" aat1.x atts2.y
+ 
+def getData(file) : 
+	json_data = open(file)
+	data = json.load(json_data)
+	json_data.close()
+	return data 
+
+def send(message, attachment) :	
+	config = getData('config.json')
+	try:
+		server = smtplib.SMTP(config['smtpsrv'], config['port'])
+		server.set_debuglevel(True) 
+
+
+		server.ehlo()  # need to say EHLO before just running straight into STARTTLS
+		server.starttls()
+
+		#Next, log in to the server
+		server.login(config['login'], config['pass'])
+
+
+		#Send the mail
+		msg = "\n Hello!" # The /n separates the message from the headers
+
+		server.sendmail(config['login'], config['tomail'], msg)
+
+		server.close()
+		print 'successfully sent the mail'
+	except:
+		print "failed to send mail"
+	return
+
+message = sys.argv[1]
+send("a","b")
